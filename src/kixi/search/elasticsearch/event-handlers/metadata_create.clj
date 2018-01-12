1c351cc19e4ab2673c5ffc6c356289bf274b4cfa
Tom Coupland <tcoupland@gmail.com>
2018-01-12 16:50:04 +0000

    Added query map definition macros and metadata upate create event consumption

--- src/kixi/search/elasticsearch/event-handlers/metadata_create.clj
+++ src/kixi/search/elasticsearch/event-handlers/metadata_create.clj
@@ -1,27 +1,27 @@
 (ns kixi.search.elasticsearch.event-handlers.metadata-create
-  (:require [clojurewerkz.elastisch.rest.index :as esi]
-            [com.stuartsierra.component :as component]
+  (:require [com.stuartsierra.component :as component]
+            [kixi.comms :as c]
             [kixi.search.elasticsearch :as es]
             [kixi.spec :refer [alias]]
-            [joplin.elasticsearch.database :refer [client]]))
+            [taoensso.timbre :as timbre :refer [info]]))
 
-(alias 'ms 'kixi.datastore.metadatastore)
-
+(alias 'md 'kixi.datastore.metadatastore)
+(alias 'cs 'kixi.datastore.communication-specs)
 
 (def index-name "kixi-datastore_file-metadata")
 (def doc-type "file-metadata")
 
 (def doc-def
-  {::ms/id es/string-stored-not_analyzed
-   ::ms/type es/string-stored-not_analyzed
-   ::ms/name es/string-autocomplete
-   ::ms/description es/string-analyzed
-   ::ms/tags es/string-autocomplete
-   ::ms/provenance {:properties {::ms/source es/string-stored-not_analyzed
+  {::md/id es/string-stored-not_analyzed
+   ::md/type es/string-stored-not_analyzed
+   ::md/name es/string-autocomplete
+   ::md/description es/string-analyzed
+   ::md/tags es/string-autocomplete
+   ::md/provenance {:properties {::md/source es/string-stored-not_analyzed
                                  :kixi.user/id es/string-stored-not_analyzed
-                                 ::ms/parent-id es/string-stored-not_analyzed
-                                 ::ms/created es/timestamp}}
-   ::ms/sharing {:properties (zipmap ms/activities
+                                 ::md/parent-id es/string-stored-not_analyzed
+                                 ::md/created es/timestamp}}
+   ::md/sharing {:properties (zipmap ms/activities
                                      (repeat es/string-stored-not_analyzed))}})
 
 (def local-es-url "http://localhost:9200")
@@ -49,5 +49,73 @@
    index-name
    doc-type
    es-url
-   (::ms/id metadata)
+   (::md/id metadata)
    metadata))
+
+
+(defmulti update-metadata-processor
+  (fn [conn update-event]
+    (::cs/file-metadata-update-type update-event)))
+
+
+(defmethod update-metadata-processor ::cs/file-metadata-created
+  [es-url update-event]
+  (let [metadata (::md/file-metadata update-event)]
+    (info "Create: " metadata)
+    (es/insert-data
+     index-name
+     doc-type
+     es-url
+     (::md/id metadata)
+     metadata)))
+
+(defmethod update-metadata-processor ::cs/file-metadata-structural-validation-checked
+  [conn update-event]
+  (info "Update: " update-event)
+)
+
+(defmethod update-metadata-processor ::cs/file-metadata-sharing-updated
+  [conn update-event]
+  (info "Update Share: " update-event)
+  )
+(defn dissoc-nonupdates
+  [md]
+  (reduce
+   (fn [acc [k v]]
+     (if (and (namespace k) (clojure.string/index-of (namespace k) ".update"))
+       (assoc acc k v)
+       acc))
+   {}
+   md))
+
+(defmethod update-metadata-processor ::cs/file-metadata-update
+  [conn update-event]
+  (info "Update: " update-event)
+  )
+
+(defn response-event
+  [r]
+  nil)
+
+(defn connection->es-url
+  [connection]
+  (str "http://" (:host connection) ":" (:port connection 9200)))
+
+(defrecord MetadataUpdate
+    [communications connection es-url started]
+  component/Lifecycle
+  (start [component]
+    (if-not started
+      (let [es-url (connection->es-url connection)]
+        (c/attach-event-handler! communications
+                                 :kixi.search/metadata-update
+                                 :kixi.datastore.file-metadata/updated
+                                 "1.0.0"
+                                 (comp response-event
+                                       (partial update-metadata-processor es-url)
+                                       :kixi.comms.event/payload))
+        (assoc component
+               :started true
+               :es-url es-url))
+      component))
+  (stop [component]))
--- src/kixi/search/query_model.clj
+++ src/kixi/search/query_model.clj
@@ -1,6 +1,7 @@
 (ns kixi.search.query-model
   (:require [clojure.spec.alpha :as s]
-            [kixi.spec :refer [alias]]))
+            [kixi.spec :refer [alias]]
+            [com.rpl.specter :as sp]))
 
 ;; TODO generate these
 
@@ -17,21 +18,83 @@
 (s/def ::msq/description string?)
 
 (s/def ::msq/meta-read
-  (s/keys :opt-un [::array/contains]))
+  (s/keys :opt-un [::contains]))
 
 (s/def ::msq/sharing
   (s/keys :opt-un [::msq/meta-read]))
 (s/def ::msq/tags
   (s/keys ))
 
-(s/def ::query
-  (s/and (s/keys :opt [::msq/name
-                       ::msq/description
-                       ::msq/sharing])
-         (s/every-kv #{::msq/name
-                       ::msq/description
-                       ::msq/sharing}
-                     (constantly true))))
+(defn query-spec-name
+  [spec]
+  (keyword (str (namespace spec) ".query") (name spec)))
+
+(defn query-spec
+  [[spec actions]]
+  (eval
+   `(s/def ~(query-spec-name spec)
+      (s/map-of ~actions
+                ~spec))))
+
+(defn query-map-spec
+  [[map-spec fields]]
+  (eval
+   `(s/def ~(query-spec-name map-spec)
+      (s/and (s/keys
+              :opt ~(mapv query-spec-name fields))
+             (s/every-kv ~(into #{} (mapv query-spec-name fields))
+                         (constantly true))))))
+
+(defn all-specs-with-actions
+  [definition-map]
+  (distinct
+   (sp/select
+    [sp/ALL (sp/if-path [sp/LAST map?]
+                        [sp/LAST sp/ALL]
+                        identity)]
+    definition-map)))
+
+(defn sub-maps-with-keys
+  [definition-map]
+  (distinct
+   (mapv (fn [[k v]]
+           [k (keys v)])
+         (sp/select
+          [sp/ALL (sp/selected? sp/LAST map?)]
+          definition-map))))
+
+(defn create-query-specs
+  [definition-map]
+  (let [query-specs (map query-spec (all-specs-with-actions definition-map))
+        map-specs (map query-map-spec (sub-maps-with-keys definition-map))]
+    (doall (concat query-specs map-specs))))
+
+(comment "Declarative structure for defining valid query commands for metadata fields.
+Keys are transformed using 'query-spec-name' which appends 'query' to the namespace,
+sets of commands become map-of specs where keys must be in the set and values must adhere to
+the orignal unmodified spec.
+
+Example:
+(def metadata->query-actions
+  {::ms/name #{:match}}
+
+Creates:
+(s/def ::msq/name
+  (map-of #{:match} :kixi.datastore.metadatastore/name))")
+(def metadata->query-actions
+  {::ms/name #{:match}
+   ::ms/sharing {::ms/meta-read #{:contains}}})
+
+(create-query-specs metadata->query-actions)
+
+(defmacro define-query-spec
+  []
+  `(s/def ::query
+     (s/and (s/keys :opt ~(mapv query-spec-name (keys metadata->query-actions)))
+            (s/every-kv ~(into #{} (mapv query-spec-name (keys metadata->query-actions)))
+                        (constantly true)))))
+
+(define-query-spec)
 
 ;; A small set of selectable fields, use when using auto complete searches
 (s/def ::fields
