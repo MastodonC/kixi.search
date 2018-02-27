c09a675c0ee6aeb68160784b919ff2b17832bb02
Tom Coupland <tcoupland@gmail.com>
2018-02-27 14:19:24 +0000

    Adding id and provenance to allows field selections (#23)

---
+++ test/kixi/search/query_model_tests.clj
@@ -0,0 +1,19 @@
+(ns kixi.search.query-model-tests
+  (:require [kixi.search.query-model :as sut]
+            [clojure.test :as t :refer [deftest is]]
+            [kixi.spec :refer [alias]]
+            [clojure.spec.alpha :as spec]))
+
+(alias 'msq 'kixi.datastore.metadatastore.query)
+(alias 'ms 'kixi.datastore.metadatastore)
+
+(deftest name-query
+  (is (spec/valid? ::sut/query-map
+                   {:query {::msq/name {:match "a"}}})))
+
+(deftest name-fields-query
+  (is (spec/valid? ::sut/query-map
+                   {:query {::msq/name {:match "a"}}
+                    :fields [::ms/name ::ms/id
+                             [::ms/provenance ::ms/created]
+                             [::ms/provenance ::ms/created-by]]})))
