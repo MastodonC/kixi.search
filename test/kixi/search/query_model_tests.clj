2c40a4c87a6154e1e58b21c7a8ca38bff1cf5a14
Tom Coupland <tcoupland@gmail.com>
2018-03-01 17:07:52 +0000

    Allow type overides for query model specs (#32)
    This allows us to accepts invalid file names as searches for file
    names. Previously this wasn't possible due to the spec for the query
    term being constrained to the normal file name spec.

--- test/kixi/search/query_model_tests.clj
+++ test/kixi/search/query_model_tests.clj
@@ -9,7 +9,9 @@
 
 (deftest name-query
   (is (spec/valid? ::sut/query-map
-                   {:query {::msq/name {:match "a"}}})))
+                   {:query {::msq/name {:match "a"}}}))
+  (is (spec/valid? ::sut/query-map
+                   {:query {::msq/name {:match "["}}})))
 
 (deftest name-fields-query
   (is (spec/valid? ::sut/query-map
@@ -17,3 +19,25 @@
                     :fields [::ms/name ::ms/id
                              [::ms/provenance ::ms/created]
                              [::ms/provenance :kixi.user/id]]})))
+
+(deftest to-homogeneous-test
+  (is (= {::ms/name {:predicates #{:match}}
+          ::ms/sharing {::ms/meta-read {:predicates #{:contains}}}}
+         (sut/to-homogeneous
+          {::ms/name #{:match}
+           ::ms/sharing {::ms/meta-read #{:contains}}})))
+  (is (= {::ms/name {:predicates #{:match}}
+          ::ms/foo {:predicates #{:match}}
+          ::ms/sharing {::ms/meta-read {:predicates #{:contains}}
+                        ::ms/bar {:predicates #{:contains}}}}
+         (sut/to-homogeneous
+          {::ms/name #{:match}
+           ::ms/foo {:predicates #{:match}}
+           ::ms/sharing {::ms/meta-read #{:contains}
+                         ::ms/bar {:predicates #{:contains}}}}))))
+
+(deftest all-specs-with-actions-test
+  (is (= '([:kixi.datastore.metadatastore/name {:predicates #{:match}}]
+           [:kixi.datastore.metadatastore/meta-read {:predicates #{:contains}}])
+         (sut/all-specs-with-actions {::ms/name {:predicates #{:match}}
+                                      ::ms/sharing {::ms/meta-read {:predicates #{:contains}}}}))))
