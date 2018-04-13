0edb1a95e68adc8fee104a177b0facff616e2e92
Matt Ford <matt@dancingfrog.co.uk>
2018-04-13 13:03:57 +0100

    Revert "Add back in bad-event hack (it seems we failed to fix it)"
    This reverts commit 643bc7ff5c6ab7811a66eb527e5e6d6d9db92f09.

--- src/kixi/search/metadata/event_handlers/update.clj
+++ src/kixi/search/metadata/event_handlers/update.clj
@@ -29,14 +29,12 @@
   [es-url index-name update-event]
   (let [metadata (::md/file-metadata update-event)]
     (info "Create: " metadata)
-    (when-not (= "891cd066-ddeb-43f7-bbe3-d854c663c4ad"
-                 (::md/id metadata))
-      (es/insert-data
-       index-name
-       doc-type
-       es-url
-       (::md/id metadata)
-       metadata))))
+    (es/insert-data
+     index-name
+     doc-type
+     es-url
+     (::md/id metadata)
+     metadata)))
 
 (defmethod update-metadata-processor ::cs/file-metadata-structural-validation-checked
   [es-url index-name update-event]
@@ -56,8 +54,6 @@
 (defmethod update-metadata-processor ::cs/file-metadata-sharing-updated
   [es-url index-name update-event]
   (info "Update Share: " update-event)
-  (when-not (= "891cd066-ddeb-43f7-bbe3-d854c663c4ad"
-               (::md/id update-event)))
   (es/apply-func index-name doc-type es-url
                  (::md/id update-event)
                  #(sharing-updater % update-event)))
